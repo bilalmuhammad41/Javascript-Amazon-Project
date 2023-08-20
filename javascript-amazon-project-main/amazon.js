e2ab1e94d25ffd3f14365a39fc5d52595c0fa424
bilal <bilal@gmail.com>
2023-08-20 15:43:33 +0500

    Made add-t0-cart btn Interactive

--- javascript-amazon-project-main/amazon.js
+++ javascript-amazon-project-main/amazon.js
@@ -46,11 +46,36 @@ products.forEach((product) =>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">Add to Cart</button>
+          <button class="add-to-cart-button js-add-to-cart button-primary " data-product-id = "${product.id}">Add to Cart</button>
         </div>
 `;
 
 } )
-console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) =>{
+    button.addEventListener('click', () => {
+      const productID = button.dataset.productId;
+      
+      let matchingItem;
+      
+      cart.forEach((item) => {
+        if(productID === item.productID){
+          matchingItem = item;
+        }
+      });
+
+      if(matchingItem){
+        matchingItem.quantity += 1;
+      } else{
+        cart.push({
+          productID: productID,
+          quantity: 1
+        });
+      }
+      console.log(cart);
+    })
+})
+''
