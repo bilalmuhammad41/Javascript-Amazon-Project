e10ae7e414a234097662f3dd9516ec179321a4c9
bilal <bilal@gmail.com>
2023-08-30 23:04:30 +0500

    Saved cart to localStorage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,12 +1,19 @@
-export let cart = [{
-  productID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-},
-{
-  productID: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1,
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
+if (!cart){
+  cart = [{
+    productID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+    },
+    {
+    productID: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+ }];
+}
+
+function saveToLocalStorage(cart){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productID){
   let matchingItem;
@@ -25,6 +32,7 @@ export function addToCart(productID){
     });
   }
   console.log(cart);
+  saveToLocalStorage(cart);
 }
 
 export function removeFromCart(productID){
@@ -36,5 +44,5 @@ export function removeFromCart(productID){
     }
   });
   cart = newCart;
-  
+  saveToLocalStorage(cart);
 }
